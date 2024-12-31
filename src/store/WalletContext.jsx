723339965d9c1e9972ba3dba8ab37021b95ca123
Biglad22 <emmanuelaberuagba22@gmail.com>
2024-12-31 19:34:30 +0100

    fixed mobile wallet connection

--- src/store/WalletContext.jsx
+++ src/store/WalletContext.jsx
@@ -1,28 +1,17 @@
 //CONTEXT PROVIDER FOR SOLANA WALLET FUNCTIONS AND STATES 
-import { useMemo } from 'react';
 import { WalletProvider, ConnectionProvider} from '@solana/wallet-adapter-react';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { clusterApiUrl } from '@solana/web3.js';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 
 export const WalletContext = ({children}) =>{
 
-    const wallets = useMemo(
-        () => [
-            new PhantomWalletAdapter(),
-            new SolflareWalletAdapter(),
-        ],
-        [] // Ensure wallets are memoized to avoid unnecessary re-creation
-    );
+    const wallets = [];
 
     const endPoint = clusterApiUrl("devnet");
 
     return (
         <ConnectionProvider endpoint={endPoint}>
-            <WalletProvider wallets={wallets} autoConnect >
-                <WalletModalProvider>
-                    {children}
-                </WalletModalProvider>
+            <WalletProvider  wallets={wallets} autoConnect >
+                {children}
             </WalletProvider>
         </ConnectionProvider>
     )
