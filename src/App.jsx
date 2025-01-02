ccd0d908d254993cfc3d5a0855b3fd1083c1a0e7
Biglad22 <emmanuelaberuagba22@gmail.com>
2025-01-02 11:32:04 +0100

    fixed mobile wallet connection

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App(){
 
     return(
         <Router>
-            <div className="bg-[#003338] main-wrapper overflow-hidden">
-                <div className="flex flex-col relative gap-0 mx-0 w-full py-2 px-[1.33rem] sm:px-[2.50rem] md:px-[3.33rem] lg:px-[4.33rem]  xl:px-[5.33rem] pt-0  min-[1500px]:px-0 min-[1500px]:w-[1300px] min-[1500px]:mx-auto max-h-full h-full">
+            <div className="bg-[#003338] main-wrapper min-h-fit">
+                <div className="flex flex-col relative gap-0 mx-0 w-full py-2 px-[1.33rem] sm:px-[2.50rem] md:px-[3.33rem] lg:px-[4.33rem]  xl:px-[5.33rem] pt-0  min-[1500px]:px-0 min-[1500px]:w-[1300px] min-[1500px]:mx-auto h-fit min-h-full">
                     <NavBar />
 
                     {/* all valid routes {AUTH (INDEX PAGE), DASHBOARD} */}
                     <Routes>
                         <Route path="/" element={<AuthPage />} />
-                        <Route path="/dashboard" element={<RoutesGuard><DashBoard className="flex-1 " /></RoutesGuard>} />
+                        <Route path="/dashboard" element={<RoutesGuard><DashBoard className="h-fit " /></RoutesGuard>} />
                         <Route path="*" element={<Navigate to="/" replace />} />
                     </Routes>
 
